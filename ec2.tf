d0375461e5333d70d2a57b8f0fa3b1a455c38638
Carlos Quintanilha <cquinta@gmail.com>
2023-06-20 18:57:20 -0300

    Incluindo a chave 2

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,11 @@
 resource "aws_instance" "web" {
+  depends_on = [
+    aws_key_pair.mtc_auth
+  ]
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   vpc_security_group_ids = var.enable_sg ? aws_security_group.optional[*].id : [data.aws_security_group.default.id]
+  key_name = aws_key_pair.mtc_auth.key_name
 
   tags = {
     Name = var.name
