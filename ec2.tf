1ca3d048f8877d31341575bff92699c6dbefd204
Carlos Quintanilha <cquinta@gmail.com>
2023-08-12 15:24:02 -0300

    ajuste eip

--- ec2.tf
+++ ec2.tf
@@ -5,9 +5,14 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = var.enable_sg ? aws_security_group.optional[*].id : [data.aws_security_group.default.id]
   key_name = var.chave
   user_data = file(var.user_data_file)
-  if var.ip_publico == "yes" {
-    eip = aws_eip.eip.id
-  }
+ 
+}
+
+resource "aws_eip" "publicip" {
+  instance = aws_instance.web.id
+  count   = var.public_ip == "yes" ? 1 : 0
+
+  
 }
 
 
--- eip.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_eip" "eip" {
-  if var.ip_publico == "yes" {
-    count = 1
-  }
-}
